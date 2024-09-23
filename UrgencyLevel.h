09ea169b8e22a1eed806bc9ccaaeceb0c7a0293d
Omer Dayan <omerdayan94@gmail.com>
2024-09-23 12:12:33 +0300

    Adding a class to represent the urgency level of the case.

---
+++ UrgencyLevel.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef URGENCYLEVEL_H
+#define URGENCYLEVEL_H
+
+class UrgencyLevel {
+public:
+    enum Level {
+        Critical = 1,
+        Urgent = 2,
+        Normal = 3
+    };
+};
+
+#endif // URGENCYLEVEL_H
