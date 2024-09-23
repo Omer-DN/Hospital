595d44bfde041cc6faabd9b9acb520160c8529f8
Omer Dayan <omerdayan94@gmail.com>
2024-09-23 21:58:34 +0300

    casting number to urgency level

--- UrgencyLevel.h
+++ UrgencyLevel.h
@@ -2,6 +2,8 @@
 #ifndef URGENCYLEVEL_H
 #define URGENCYLEVEL_H
 
+#include <string>
+
 class UrgencyLevel {
 public:
     enum Level {
@@ -9,6 +11,16 @@ public:
         Urgent = 2,
         Normal = 3
     };
+
+    // הוספת פונקציה להמרת רמת דחיפות למחרוזת
+    static std::string toString(Level level) {
+        switch (level) {
+        case Critical: return "Critical";
+        case Urgent: return "Urgent";
+        case Normal: return "Normal";
+        default: return "Unknown";
+        }
+    }
 };
 
 #endif // URGENCYLEVEL_H
