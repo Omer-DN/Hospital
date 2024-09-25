4adc3ca57c14e027af92148bac0ea66ecf5f1752
Omer-DN <75830567+Omer-DN@users.noreply.github.com>
2024-09-25 21:42:05 +0300

    Cleaning the code from "std:: "

--- Patients.cpp
+++ Patients.cpp
@@ -7,33 +7,23 @@ void Patients::addPatient(const Patient& patient) {
     patientList.push_back(patient);
 }
 
+
 void Patients::sortPatients() {
-    std::sort(patientList.begin(), patientList.end(), [](const Patient& a, const Patient& b) {
-        return static_cast<int>(a.getUrgency()) < static_cast<int>(b.getUrgency());
+    sort(patientList.begin(), patientList.end(), [](const Patient& a, const Patient& b) {
+        return a.getUrgency() != b.getUrgency() ? static_cast<int>(a.getUrgency()) < static_cast<int>(b.getUrgency()) : a.getName() < b.getName();
         });
 }
 
-void Patients::writeToCSV(const std::string& filename) const {
-    std::ofstream outFile(filename);
-    if (outFile.is_open()) {
-        outFile << "Name,Condition,Urgency\n";
-        for (const auto& patient : patientList) {
-            outFile << patient.getName() << ","
-                << patient.getCondition() << ","
-                << static_cast<int>(patient.getUrgency()) << "\n";
-        }
-        outFile.close();
-    }
-}
 
 void Patients::printPatients() const {
     for (const auto& patient : patientList) {
-        std::cout << "Name: " << patient.getName()
+        cout << "Name: " << patient.getName()
             << ", Condition: " << patient.getCondition()
             << ", Urgency: " << static_cast<int>(patient.getUrgency()) << std::endl;
     }
 }
 
-std::vector<Patient> Patients::getPatientList() const {
+
+vector<Patient> Patients::getPatientList() const {
     return patientList;
 }
