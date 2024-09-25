47fbaddefda2eda737d7473583cb5ae9db6111cc
Omer-DN <75830567+Omer-DN@users.noreply.github.com>
2024-09-25 21:12:43 +0300

    Cleaning the code from "std:: "

--- PatientDataHandler.cpp
+++ PatientDataHandler.cpp
@@ -2,13 +2,12 @@
 #include <iostream>
 
 void PatientDataHandler::printPatients(const Patients& patients) const {
-    const std::vector<Patient>& patientList = patients.getPatientList();
+    const vector<Patient>& patientList = patients.getPatientList();
 
     for (const Patient& patient : patientList) {
-        std::cout << "Patient ID: " << patient.getId()
+        cout << "Patient ID: " << patient.getId()
             << ", Name: " << patient.getName()
             << ", Condition: " << patient.getCondition()
-            << ", Age: " << patient.getAge() << std::endl;
+            << ", Age: " << patient.getAge() <<endl;
     }
-
 }
