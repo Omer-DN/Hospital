2d06aa0cdad4313fa1e7f3d7453a6f9c981301b3
Omer-DN <75830567+Omer-DN@users.noreply.github.com>
2024-09-25 19:53:38 +0300

    Delete unused code

--- EmergencyQueue.cpp
+++ EmergencyQueue.cpp
@@ -7,25 +7,7 @@ using namespace std;
 
 unordered_map<string, int> EmergencyQueue::conditionWaitTimes;
 
-void EmergencyQueue::initializeWaitTimes() {
-    conditionWaitTimes["Chest pain"] = 10;
-    conditionWaitTimes["Sudden difficulty breathing"] = 10;
-    conditionWaitTimes["Severe injury"] = 10;
-    conditionWaitTimes["Severe bleeding"] = 10;
-    conditionWaitTimes["Signs of stroke"] = 10;
-    conditionWaitTimes["Seizures"] = 10;
-    conditionWaitTimes["Suspected fractures"] = 60;
-    conditionWaitTimes["Very high fever"] = 120;
-    conditionWaitTimes["Poisoning"] = 10;
-    conditionWaitTimes["Severe allergic reactions"] = 10;
-    conditionWaitTimes["Persistent vomiting"] = 240;
-    conditionWaitTimes["Severe abdominal pain"] = 120;
-    conditionWaitTimes["Eye injuries"] = 120;
-    conditionWaitTimes["Severe mental health crisis"] = 10;
-    conditionWaitTimes["Loss of consciousness"] = 10;
-    conditionWaitTimes["Sudden swelling or pain in limbs"] = 120;
-    conditionWaitTimes["Animal or snake bites"] = 10;
-}
+void EmergencyQueue::initializeWaitTimes() {}
 
 int EmergencyQueue::getWaitTime(const string& condition) {
     if (conditionWaitTimes.find(condition) != conditionWaitTimes.end()) {
@@ -37,6 +19,6 @@ int EmergencyQueue::getWaitTime(const string& condition) {
 void EmergencyQueue::printWaitTimes() {
     cout << "Emergency Cases and Waiting Times (in minutes):\n";
     for (const auto& caseTime : conditionWaitTimes) {
-        cout <<endl<< caseTime.first << ": " << caseTime.second << " minutes" << endl;
+        cout << endl << caseTime.first << ": " << caseTime.second << " minutes" << endl;
     }
 }
